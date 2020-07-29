d79bdcedfc64a33cefc3fdb286648555e1d8f44a
smishra11 <msubhasish11@gmail.com>
2020-07-29 19:45:29 +0530

    Sending user data to Firebase

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -66,9 +66,14 @@ class ContactData extends Component {
   orderHandler = (event) => {
     event.preventDefault();
     this.setState( { loading: true } );
+    const formData = {};
+    for(let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+    }
       const order = {
         ingredients: this.props.ingredients,
-        price: this.props.price
+        price: this.props.price,
+        orderData: formData
       }
       axios.post( '/orders.json', order )
         .then( response => {
@@ -80,6 +85,18 @@ class ContactData extends Component {
         } );
   }
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier] 
+    };
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm});
+  }
+
     render () {
       const formElementsArray = [];
       for(let key in this.state.orderForm) {
@@ -89,15 +106,16 @@ class ContactData extends Component {
         });
       }
       let form = (
-        <form>
+        <form onSubmit={this.orderHandler}>
             {formElementsArray.map(formElement => (
               <Input 
                 key={formElement.id}
                 elementType={formElement.config.elementType}
                 elementConfig={formElement.config.elementConfig}
-                value={formElement.config.value} />
+                value={formElement.config.value}
+                changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
             ))}
-            <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
+            <Button btnType='Success'>ORDER</Button>
         </form>
       );
       if(this.state.loading) {
